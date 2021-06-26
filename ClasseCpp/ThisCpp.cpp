cb85953fce94bb9cd55a49e34cb08d53cb643d27
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-06-25 23:17:13 -0300

    Atualizado a pasta classeCpp

--- ClasseCpp/ThisCpp.cpp
+++ ClasseCpp/ThisCpp.cpp
@@ -4,24 +4,27 @@ class A
 {
     int x;
     public:
-        int qual()
-        {
-            return this->x;
-        }
-
-        void set_x(int x)
-        {
-            this->x = x;
-        }
+        int get_x();
+        void set_x(int x);
 };
+
+int A::get_x()
+{
+    return this->x;
+}
+
+void A::set_x(int x)
+{
+    this->x = x;
+}
 
 int main(int argc, char const *argv[])
 {
     A obj1, obj2;
     obj1.set_x(23);
     obj2.set_x(33);
-    std::cout << "Valor de x do obj1 " << obj1.qual() << "\n";
-    std::cout << "Valor de x do obj1 " << obj2.qual() << "\n";
+    std::cout << "Valor de x do obj1 " << obj1.get_x() << "\n";
+    std::cout << "Valor de x do obj1 " << obj2.get_x() << "\n";
 
     return 0;
 }
