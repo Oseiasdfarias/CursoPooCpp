cb06cac590f3c9cedafdb1af8978ac1ab5f4a3ab
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-06-27 12:01:25 -0300

    Exercicio de template de polimosfirmos

--- SobrecargasDeOperadores/Polimofismo/PolimorfismoTemplate.cpp
+++ SobrecargasDeOperadores/Polimofismo/PolimorfismoTemplate.cpp
@@ -6,6 +6,19 @@ X soma( X a, X b)
     return a + b;
 }
 
+template < typename D >
+void maior(D a, D b)
+{
+    if (a > b)
+    { std::cout << a << " > " << b << "\n"; }
+
+    else if (a == b)
+    { std::cout << a << " = " << b << "\n"; }
+
+    else
+    { std::cout << b << " > " << a << "\n"; }
+}
+
 int main(int argc, char const *argv[])
 {
     int i1 = 12, i2 = 20;
@@ -13,5 +26,11 @@ int main(int argc, char const *argv[])
 
     double d1 = 12.23, d2 = 12.77;
     std::cout << "Soma de " << d1 << " + " << d2 << " = " << soma(d1, d2) << "\n\n";
+
+    maior(i1, i2);
+    maior(i1, i1);
+    maior(d1, d2);
+    maior(d1, d1);
+    
     return 0;
 }
