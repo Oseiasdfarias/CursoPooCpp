5c1a18648dc5e24cc5f7840146a3ce4384626ea4
Oséias D. Farias <52744236+Oseiasdfarias@users.noreply.github.com>
2021-06-29 13:17:16 -0300

    atualizaçao das pastas

--- SobrecargasDeOperadores/Polimofismo/PolimorfismoTemplateClass.cpp
+++ SobrecargasDeOperadores/Polimofismo/PolimorfismoTemplateClass.cpp
@@ -13,11 +13,11 @@ class Classe
 int main(int argc, char const *argv[])
 {
     Classe <int> ObjI;
-    std::cout << "Valor de tipoGenerico: " << ObjI.tipoGenerico << "\n";
+    std::cout << "\nValor de tipoGenerico: " << ObjI.tipoGenerico << "\n";
 
     Classe <float> ObjD( 2.8383, 2 );
     std::cout << "Valor de tipoGenerico: " << ObjD.tipoGenerico << "\n";
-    std::cout << "Valor de tipoGenerico: " << ObjD.y << "\n";
+    std::cout << "Valor de tipoGenerico: " << ObjD.y << "\n\n";
 
     return 0;
 }
